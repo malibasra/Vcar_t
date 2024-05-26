2b147c7cd44b842db2e5e7bfa3093f0ee894b57b
Muhammad Ali Basra <167224248+malibasra@users.noreply.github.com>
2024-05-26 20:05:01 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Main from './Comm/Main';
+
+function App() {
+  return (
+    <>
+    <div className='bg-primary-subtle text-center'>
+      {/*<div className='container'>*/}
+      <Main/>
+    </div>
+    {/*</div>*/}
+    </>
+  );
+}
+
+export default App;
